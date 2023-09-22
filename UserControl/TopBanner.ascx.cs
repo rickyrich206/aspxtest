e893a4494a4a8eb7db03a0f07b1608991d102d8b
rickyrich206 <ricky.r@quadrasystems.net>
2023-09-22 11:22:12 +0530

    firstcommit

---
+++ UserControl/TopBanner.ascx.cs
@@ -0,0 +1,23 @@
+﻿using PMSCLS.Model;
+using System;
+using System.Web.UI;
+
+namespace PMS.UserControl
+{
+    public partial class TopBanner : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SessionL ssl = new PMSCLS.ILogin().GetCookieVal();
+            if (ssl.UserId == null || ssl.UserId == "-1")
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "A", "<script>LoginCalldialog('Your Session has expired');</script>", false);
+                return;
+            }
+            if (!Page.IsPostBack)
+            {
+                lblUserName.InnerText = "Welcome : " + ssl.UserName.ToString();
+            }
+        }
+    }
+}
